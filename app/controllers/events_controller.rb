e02877604e61cf42addbd0f08f66774f41c43a24
Chris Bellairs <c.m.bellairs@gmail.com>
2021-03-02 21:01:07 +0000

    Added sql query logic to index action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,11 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    sql_query = "events.date ILIKE :query"
+
+    if params[:query].present?
+      @events = Event.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @events = Event.all
+    end
   end
 end
