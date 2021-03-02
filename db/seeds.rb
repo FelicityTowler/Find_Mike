d05e80654824d525f94cbd9e8ffc43517f24f2ee
Freddie Fryer <freddiefryercode@gmail.com>
2021-03-02 15:33:01 +0000

    database will clean when run

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+Booking.destroy_all
+User.destroy_all
+Event.destroy_all
+Venue.destroy_all
+puts "Database Cleaned"
+
