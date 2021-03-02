2381b130861f618b239f79cdc43f88f6c0713289
Chris Bellairs <c.m.bellairs@gmail.com>
2021-03-02 21:15:05 +0000

    Added a date form to the search, added a view all button to home, added a no events found logic.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,7 @@
 class EventsController < ApplicationController
   def index
-    sql_query = "events.date ILIKE :query"
-
     if params[:query].present?
-      @events = Event.where(sql_query, query: "%#{params[:query]}%")
+      @events = Event.where(date: params[:query])
     else
       @events = Event.all
     end
