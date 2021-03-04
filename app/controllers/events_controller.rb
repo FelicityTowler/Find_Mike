8340474de829a1a256729cbdf3e4940a39ac8ce8
Chris Bellairs <c.m.bellairs@gmail.com>
2021-03-04 19:42:57 +0000

    Added events show action and created view with all require info

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,14 @@ class EventsController < ApplicationController
   def index
     if params[:query].present? && params[:availability].present?
       @events = Event.where(["date = ? and available_spots > ?", params[:query], 0])
-
-      # date: params[:query], available_spots: true
     elsif params[:query].present?
       @events = Event.where(["date = ?", params[:query]])
     else
       @events = Event.all
     end
+  end
+
+  def show
+    @event = Event.find(params[:id])
   end
 end
