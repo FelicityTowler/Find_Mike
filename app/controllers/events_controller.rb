98b42f65d0b21696d5a712ef1dfe2fad492e01e2
Chris Bellairs <c.m.bellairs@gmail.com>
2021-03-04 19:22:33 +0000

    Added availability checkbox to the search and update controller logic to allow it to work.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,11 @@
 class EventsController < ApplicationController
   def index
-    if params[:query].present?
-      @events = Event.where(date: params[:query])
+    if params[:query].present? && params[:availability].present?
+      @events = Event.where(["date = ? and available_spots > ?", params[:query], 0])
+
+      # date: params[:query], available_spots: true
+    elsif params[:query].present?
+      @events = Event.where(["date = ?", params[:query]])
     else
       @events = Event.all
     end
