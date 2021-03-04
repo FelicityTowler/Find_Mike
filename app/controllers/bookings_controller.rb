f07e26e13e40f76d8fd7ca4f167493a86960c6f3
Matt Woods <woodsmj17@gmail.com>
2021-03-04 20:20:46 +0000

    added new/create to controller and new view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+
+  def new
+    @event = Event.find(params[:event_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @booking = Booking.new(booking_params)
+    @booking.event = @event
+    @booking.user = current_user
+    if @booking.save
+      redirect_to event_booking_path(@event, @booking)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:event, :user)
+  end
 end
