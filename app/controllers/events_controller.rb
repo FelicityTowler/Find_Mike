8ba77c73496db26fcff9d1c3704396a94fbcfaf2
Freddie Fryer <freddiefryercode@gmail.com>
2021-03-06 13:09:23 +0000

    added some test code to events controller called event_find

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,22 @@
 class EventsController < ApplicationController
+  #test code for finding specific events
+  def event_find(location, radius, date)
+    #find all events on a certain date
+    @events = Event.where(["date = ? and available_spots > ? and cancelled = ?", params[:query], 0, false])
+    @locations = []
+    @markers = []
+    @events.geocoded.each do |event|
+      if event.venue.near(location, radius)
+        @locations << event
+        @markers << {
+        lat: event.venue.latitude,
+        lng: event.venue.longitude
+        }
+      end
+      @events = @locations
+    end
+  end
+
   def index
     if params[:query].present? && params[:availability].present?
       @events = Event.where(["date = ? and available_spots > ? and cancelled = ?", params[:query], 0, false])
