7aaf5cfb94e0b55547de320244374b8aead7c40a
Matt Woods <woodsmj17@gmail.com>
2021-03-07 15:50:32 +0000

    added curtains and spot light, but still need new image and content behind curtains

---
+++ app/javascript/components/spotlight.js
@@ -0,0 +1,21 @@
+
+const homepageSpotlight = () => {
+var spotlight = document.querySelector('.spotlight');
+var isUserMotion = false;
+  function move(e) {
+    if (e.clientX || e.touches) {
+      isUserMotion = true;
+      spotlight.style.setProperty('--x', (e.clientX || e.touches[0].clientX)+'px');
+      spotlight.style.setProperty('--y', (e.clientY || e.touches[0].clientY)+'px');
+    }
+  }
+
+  if (window.PointerEvent) {
+    spotlight.addEventListener('pointermove', move);
+  } else {
+    spotlight.addEventListener('touchmove', move);
+    spotlight.addEventListener('mousemove', move);
+  }
+}
+
+export { homepageSpotlight };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,9 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { homepageSpotlight } from '../components/spotlight';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
-  // initSelect2();
+  homepageSpotlight();
 });
