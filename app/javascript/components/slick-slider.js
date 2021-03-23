ccb8e50cedd1db703cae5aaa2f83bb46dbe065fe
Matt Woods <woodsmj17@gmail.com>
2021-03-23 17:00:57 +0000

    Added some styling to event show page and slider

--- app/javascript/components/slick-slider.js
+++ app/javascript/components/slick-slider.js
@@ -10,5 +10,16 @@ $(document).ready(function(){
     prevArrow: '<i class="fas fa-chevron-left slick-prev"></i>',
   });
 });
+$(document).ready(function(){
+  $('.scrollable-card-list-performers').slick({
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  focusOnSelect: true,
+  nextArrow: '<i class="fas fa-chevron-right slick-next"></i>',
+    prevArrow: '<i class="fas fa-chevron-left slick-prev"></i>',
+  });
+});
 }
 export { slickSlider };
